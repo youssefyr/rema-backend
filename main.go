d9aa1297b8bb9d0f1f229d61d8bb1ac233c5cf81
yossafi <yossef0110215@gmail.com>
2024-09-06 14:26:23 +0300

    a

--- main.go
+++ main.go
@@ -6,10 +6,8 @@ import (
 	"fmt"
 	"log"
 
-	// adapt "demo" to your module name if it differs
-	"rema/db"
-
 	"net/http"
+	"rema/db"
 
 	"github.com/graphql-go/graphql"
 	"github.com/graphql-go/handler"
@@ -24,6 +22,7 @@ func main() {
 
 // run is a function that initializes the database client, creates a user, and starts the GraphQL server.
 // It returns an error if any operation fails.
+// funcction  run () error
 
 func run() error {
 	client := db.NewClient()
@@ -198,164 +197,3 @@ func run() error {
 //
 // fmt.Println("The user's tasks are null")
 // }
-
-func bruteForceStrucToMap(s interface{}, inputType string) map[string]interface{} {
-	switch inputType {
-
-	case "User":
-		user := s.(*db.UserModel)
-		return map[string]interface{}{
-			"id":        user.ID,
-			"name":      user.Name,
-			"createdAt": user.CreatedAt,
-			"updatedAt": user.UpdatedAt,
-			"coins":     user.Coins,
-			"xp":        user.Xp,
-			"tasks":     user.Tasks,
-			"shopItems": user.Shopitems,
-		}
-	case "Task":
-		task := s.(*db.TaskModel)
-		return map[string]interface{}{
-			"id":             task.ID,
-			"name":           task.Title,
-			"createdAt":      task.CreatedAt,
-			"updatedAt":      task.UpdatedAt,
-			"title":          task.Title,
-			"description":    task.Description,
-			"completed":      task.Completed,
-			"user":           task.User,
-			"repeatable":     task.Repeatable,
-			"repeatduration": task.Repeatduration,
-		}
-	case "Shopitem":
-		shopItem := s.(*db.ShopItemModel)
-		return map[string]interface{}{
-			"id":        shopItem.ID,
-			"name":      shopItem.Name,
-			"createdAt": shopItem.CreatedAt,
-			"updatedAt": shopItem.UpdatedAt,
-			"price":     shopItem.Price,
-			"quantity":  shopItem.Quantity,
-			"user":      shopItem.User,
-		}
-	default:
-		return nil
-	}
-}
-
-func graphqlObjectCreator() (userType, taskType, shopItemType, baseUserType *graphql.Object) {
-
-	baseUserType = graphql.NewObject(graphql.ObjectConfig{
-		Name: "BaseUser",
-		Fields: graphql.Fields{
-			"id": &graphql.Field{
-				Type: graphql.String,
-			},
-			"name": &graphql.Field{
-				Type: graphql.String,
-			},
-			"createdAt": &graphql.Field{
-				Type: graphql.DateTime,
-			},
-			"updatedAt": &graphql.Field{
-				Type: graphql.DateTime,
-			},
-			"coins": &graphql.Field{
-				Type: graphql.Int,
-			},
-			"xp": &graphql.Field{
-				Type: graphql.Int,
-			},
-		},
-	})
-
-	taskType = graphql.NewObject(graphql.ObjectConfig{
-		Name: "Task",
-		Fields: graphql.Fields{
-			"id": &graphql.Field{
-				Type: graphql.String,
-			},
-			"createdAt": &graphql.Field{
-				Type: graphql.DateTime,
-			},
-			"updatedAt": &graphql.Field{
-				Type: graphql.DateTime,
-			},
-			"title": &graphql.Field{
-				Type: graphql.String,
-			},
-			"description": &graphql.Field{
-				Type: graphql.String,
-			},
-			"completed": &graphql.Field{
-				Type: graphql.Boolean,
-			},
-			"user": &graphql.Field{
-				Type: baseUserType,
-			},
-			"repeatable": &graphql.Field{
-				Type: graphql.Boolean,
-			},
-			"repeatduration": &graphql.Field{
-				Type: graphql.Int,
-			},
-		},
-	})
-
-	shopItemType = graphql.NewObject(graphql.ObjectConfig{
-		Name: "Shopitem",
-		Fields: graphql.Fields{
-			"id": &graphql.Field{
-				Type: graphql.String,
-			},
-			"createdAt": &graphql.Field{
-				Type: graphql.DateTime,
-			},
-			"updatedAt": &graphql.Field{
-				Type: graphql.DateTime,
-			},
-			"price": &graphql.Field{
-				Type: graphql.Float,
-			},
-			"quantity": &graphql.Field{
-				Type: graphql.Int,
-			},
-			"user": &graphql.Field{
-				Type: baseUserType,
-			},
-		},
-	})
-
-	userType = graphql.NewObject(graphql.ObjectConfig{
-		Name: "User",
-		Fields: graphql.Fields{
-			"id": &graphql.Field{
-				Type: graphql.String,
-			},
-			"name": &graphql.Field{
-				Type: graphql.String,
-			},
-			"createdAt": &graphql.Field{
-				Type: graphql.DateTime,
-			},
-			"updatedAt": &graphql.Field{
-				Type: graphql.DateTime,
-			},
-			"coins": &graphql.Field{
-				Type: graphql.Int,
-			},
-			"xp": &graphql.Field{
-				Type: graphql.Int,
-			},
-			"tasks": &graphql.Field{
-				Type: taskType,
-			},
-			"shopItems": &graphql.Field{
-				Type: shopItemType,
-			},
-		},
-	})
-
-	return userType, taskType, shopItemType, baseUserType
-}
